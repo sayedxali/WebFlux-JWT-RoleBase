75c58e7e49e12d9ffff3836626d5d62915c8bcb3
SayedAli <learn.mike.helloworld@gmail.com>
2023-07-13 20:01:45 +0430

    Initial commit

---
+++ src/test/java/com/example/mediumjwtwebflux/MediumJwtWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediumjwtwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumJwtWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
